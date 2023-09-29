438d13e4539230a25d96943760d31c9a054229f5
Alex27197 <koganalexr@gmail.com>
2023-09-29 17:45:33 +0300

    add validation in register controller

--- backend/controllers/AuthControllers.js
+++ backend/controllers/AuthControllers.js
@@ -4,38 +4,52 @@ const jwt = require("jsonwebtoken");
 
 const registerController = async (req, res) => {
   const { name, email, password, isAdmin } = req.body;
-  const userAvatar = req.file.path; // This is the Cloudinary URL
+  const userAvatar = req.file ? req.file.path : null;
+  let errors = {};
+  try {
+    if (!email || email.trim() === "") {
+      errors.emailError = "Email Is Required";
+    }
+    if (!name || name.trim() === "") {
+      errors.nameError = "Name Is Required";
+    }
+    if (!password || password.trim() === "" || password.length < 6) {
+      errors.passwordError = "Password Is Required";
+    }
+    if (errors) {
+      return res.status(400).json({ errors });
+    }
+    const userExist = await UserModel.findOne({ email });
 
-  if (!name) {
-    return res.status(400).json({ message: "Name Is Required" });
+    if (userExist) {
+      return res.status(400).json({
+        message: "This email has already been registered",
+        success: false,
+      });
+    }
+
+    const hashPass = await bcrypt.hash(password, 10); // Added await here
+
+    const user = new UserModel({
+      name,
+      email,
+      password: hashPass,
+      isAdmin,
+      userAvatar,
+    });
+
+    await user.save();
+
+    res.status(201).json({
+      message: "User Created Successfully",
+      success: true,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "User Create Failed",
+      success: false,
+    });
   }
-  if (!password) {
-    return res.status(400).json({ message: "Password Is Required" });
-  }
-
-  const userExist = await UserModel.findOne({ email });
-
-  if (userExist) {
-    return res
-      .status(400)
-      .json({ message: "This email has already been registered" });
-  }
-
-  const hashPass = await bcrypt.hash(password, 10); // Added await here
-
-  const user = new UserModel({
-    name,
-    email,
-    password: hashPass,
-    isAdmin,
-    userAvatar,
-  });
-
-  await user.save();
-
-  res.status(201).json({
-    message: "User Created Successfully",
-  });
 };
 
 const loginController = async (req, res) => {
